eb5fb0c27c8403a8acd108871188f56e43cd814c
Typed Null <ghostdarktype@gmail.com>
2023-12-22 16:47:13 +0100

    leitor.html finalized

---
+++ server.py
@@ -0,0 +1,12 @@
+# server.py
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
